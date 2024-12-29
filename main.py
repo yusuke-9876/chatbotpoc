a038209c9ac93ed8835c5facd0db0993928255bf
yusuke-9876 <nba070639@gmail.com>
2024-12-29 13:37:32 +0900

    newme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Github: https://github.com/naotaka1128/llm_app_codes/chapter05/part1/main.py
-
-import traceback
+import tiktoken
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -9,10 +7,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 
 ###### dotenv を利用しない場合は消してください ######
 try:
@@ -24,107 +18,149 @@ except ImportError:
 ################################################
 
 
-SUMMARIZE_PROMPT = """以下のコンテンツについて、内容を300文字程度でわかりやすく要約してください。
-
-========
-
-{content}
-
-========
-
-日本語で書いてね！
-"""
+MODEL_PRICES = {
+    "input": {
+        "gpt-3.5-turbo": 0.5 / 1_000_000,
+        "gpt-4o": 5 / 1_000_000,
+        "claude-3-5-sonnet-20240620": 3 / 1_000_000,
+        "gemini-1.5-pro-latest": 3.5 / 1_000_000
+    },
+    "output": {
+        "gpt-3.5-turbo": 1.5 / 1_000_000,
+        "gpt-4o": 15 / 1_000_000,
+        "claude-3-5-sonnet-20240620": 15 / 1_000_000,
+        "gemini-1.5-pro-latest": 10.5 / 1_000_000
+    }
+}
 
 
 def init_page():
     st.set_page_config(
-        page_title="Website Summarizer",
+        page_title="My Great ChatGPT",
         page_icon="🤗"
     )
-    st.header("Website Summarizer 🤗")
+    st.header("My Great ChatGPT 🤗")
     st.sidebar.title("Options")
 
 
-def select_model(temperature=0):
+def init_messages():
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+    # clear_button が押された場合や message_history がまだ存在しない場合に初期化
+    if clear_button or "message_history" not in st.session_state:
+        st.session_state.message_history = [
+            ("system", "You are a helpful assistant.")
+        ]
+
+
+def select_model():
+    # スライダーを追加し、temperatureを0から2までの範囲で選択可能にする
+    # 初期値は0.0、刻み幅は0.01とする
+    temperature = st.sidebar.slider(
+        "Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+
     models = ("GPT-3.5", "GPT-4", "Claude 3.5 Sonnet", "Gemini 1.5 Pro")
     model = st.sidebar.radio("Choose a model:", models)
     if model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo"
         return ChatOpenAI(
             temperature=temperature,
-            model_name="gpt-3.5-turbo"
+            model_name=st.session_state.model_name
         )
     elif model == "GPT-4":
+        st.session_state.model_name = "gpt-4o"
         return ChatOpenAI(
             temperature=temperature,
-            model_name="gpt-4o"
+            model_name=st.session_state.model_name
         )
     elif model == "Claude 3.5 Sonnet":
+        st.session_state.model_name = "claude-3-5-sonnet-20240620"
         return ChatAnthropic(
             temperature=temperature,
-            model_name="claude-3-5-sonnet-20240620"
+            model_name=st.session_state.model_name
         )
     elif model == "Gemini 1.5 Pro":
+        st.session_state.model_name = "gemini-1.5-pro-latest"
         return ChatGoogleGenerativeAI(
             temperature=temperature,
-            model="gemini-1.5-pro-latest"
+            model=st.session_state.model_name
         )
 
 
 def init_chain():
-    llm = select_model()
+    st.session_state.llm = select_model()
     prompt = ChatPromptTemplate.from_messages([
-        ("user", SUMMARIZE_PROMPT),
+        *st.session_state.message_history,
+        ("user", "{user_input}")  # ここにあとでユーザーの入力が入る
     ])
     output_parser = StrOutputParser()
-    chain = prompt | llm | output_parser
-    return chain
+    return prompt | st.session_state.llm | output_parser
 
 
-def validate_url(url):
-    """ URLが有効かどうかを判定する関数 """
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except ValueError:
-        return False
+def get_message_counts(text):
+    if "gemini" in st.session_state.model_name:
+        return st.session_state.llm.get_num_tokens(text)
+    else:
+        # Claude 3 はトークナイザーを公開していないので、tiktoken を使ってトークン数をカウント
+        # これは正確なトークン数ではないが、大体のトークン数をカウントすることができる
+        if "gpt" in st.session_state.model_name:
+            encoding = tiktoken.encoding_for_model(st.session_state.model_name)
+        else:
+            encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")  # 仮のものを利用
+        return len(encoding.encode(text))
 
 
-def get_content(url):
-    try:
-        with st.spinner("Fetching Website ..."):
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # なるべく本文の可能性が高い要素を取得する
-            if soup.main:
-                return soup.main.get_text()
-            elif soup.article:
-                return soup.article.get_text()
-            else:
-                return soup.body.get_text()
-    except:
-        st.write(traceback.format_exc())  # エラーが発生した場合はエラー内容を表示
-        return None
+def calc_and_display_costs():
+    output_count = 0
+    input_count = 0
+    for role, message in st.session_state.message_history:
+        # tiktoken でトークン数をカウント
+        token_count = get_message_counts(message)
+        if role == "ai":
+            output_count += token_count
+        else:
+            input_count += token_count
+
+    # 初期状態で System Message のみが履歴に入っている場合はまだAPIコールが行われていない
+    if len(st.session_state.message_history) == 1:
+        return
+
+    input_cost = MODEL_PRICES['input'][st.session_state.model_name] * input_count
+    output_cost = MODEL_PRICES['output'][st.session_state.model_name] * output_count
+    if "gemini" in st.session_state.model_name and (input_count + output_count) > 128000:
+        input_cost *= 2
+        output_cost *= 2
+
+    cost = output_cost + input_cost
+
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${cost:.5f}**")
+    st.sidebar.markdown(f"- Input cost: ${input_cost:.5f}")
+    st.sidebar.markdown(f"- Output cost: ${output_cost:.5f}")
 
 
 def main():
     init_page()
+    init_messages()
     chain = init_chain()
 
-    # ユーザーの入力を監視
-    if url := st.text_input("URL: ", key="input"):
-        is_valid_url = validate_url(url)
-        if not is_valid_url:
-            st.write('Please input valid url')
-        else:
-            if content := get_content(url):
-                st.markdown("## Summary")
-                st.write_stream(chain.stream({"content": content}))
-                st.markdown("---")
-                st.markdown("## Original Text")
-                st.write(content)
+    # チャット履歴の表示 (第2章から少し位置が変更になっているので注意)
+    for role, message in st.session_state.get("message_history", []):
+        st.chat_message(role).markdown(message)
 
-    # コストを表示する場合は第3章と同じ実装を追加してください
-    # calc_and_display_costs()
+    # ユーザーの入力を監視
+    if user_input := st.chat_input("聞きたいことを入力してね！"):
+        st.chat_message('user').markdown(user_input)
+
+        # LLMの返答を Streaming 表示する
+        with st.chat_message('ai'):
+            response = st.write_stream(chain.stream({"user_input": user_input}))
+
+        # チャット履歴に追加
+        st.session_state.message_history.append(("user", user_input))
+        st.session_state.message_history.append(("ai", response))
+
+    # コストを計算して表示
+    calc_and_display_costs()
 
 
 if __name__ == '__main__':
--- my_first_app.py
+++
@@ -1,2 +0,0 @@
-import streamlit as st
-st.write("Hello world. Let's learn how to bulid a AI-basd app together")
